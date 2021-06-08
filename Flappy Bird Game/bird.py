7aca3b2dafe0124bd280882e7d8c8f4779051587
Denise Kersjes <denise.kersjes@hotmail.com>
2021-06-08 21:18:44 +0200

    Updated documentation

--- Flappy Bird Game/bird.py
+++ Flappy Bird Game/bird.py
@@ -4,36 +4,81 @@ from defs import *
 from nnet import Nnet
 import numpy as np
 
-class Bird():
-    def __init__(self, gameDisplay): #initialize bird and its features
+
+class Bird:
+    """ Object to store the information of bird movements
+    """
+
+    def __init__(self, gameDisplay):
+        """ Initialize the bird movement class
+
+        :param gameDisplay: pygame window size
+        """
+
+        # Set the display width and height
         self.gameDisplay = gameDisplay
-        self.state = BIRD_ALIVE #Bird starts alive
-        self.img = pygame.image.load(BIRD_FILENAME) #Load image of bird
-        self.rect = self.img.get_rect() #Is used to find the position of bird
-        self.speed = 0 #start with 0 speed
-        self.time_lived = 0 #start with no time lived (This is for the AI to see if its doing well
-        self.set_position(BIRD_START_X, BIRD_START_Y) #Startposition
+
+        # Set the bird on living
+        self.state = BIRD_ALIVE
+
+        # Load the image of the bird
+        self.img = pygame.image.load(BIRD_FILENAME)
+
+        # Get the position of the bird image
+        self.rect = self.img.get_rect()
+
+        # Set the start speed at 0
+        self.speed = 0
+
+        # Set the start living time at 0 (This is for the AI to see if it works well)
+        self.time_lived = 0
+
+        # Set the startpositiom of the bird
+        self.set_position(BIRD_START_X, BIRD_START_Y)
+
         # Initialize the neural network class with the given parameters
         self.nnet = Nnet(NNET_INPUTS, NNET_HIDDEN, NNET_OUTPUTS)
+
+        # Set the bird fitness at 0
         self.fitness = 0
 
     def set_position(self, x, y):
+        """ Function to get the initially position of the bird
+
+        :param x: integer, x-position of the bird
+        :param y: integer, y-positiion of the bird
+        """
         self.rect.centerx = x
         self.rect.centery = y
 
-    def move(self, dt): #When moving
-        distance = 0 #empty distance
-        new_speed = 0 #empty speed
+    def move(self, dt):
+        """  Function to update the bird movement variables
 
-        distance = (self.speed * dt) + (0.5 * GRAVITY * dt * dt) #s = ut + 0.5at^2
-        new_speed = self.speed + (GRAVITY * dt) #v = u + at
+        :param dt: integer, time movement in milliseconds
+        """
 
-        self.rect.centery += distance #Set the position of the bird on the y axis
-        self.speed = new_speed #Set speed of the bird
+        # When moving, restart the distance and speed
+        distance = 0
+        new_speed = 0
 
-        if self.rect.top < 0: #If the bird goes above the screen
-            self.rect.top = 0 #stay in screen
-            self.speed = 0 #set speed to 0
+        # Calculate the distance with s = ut + 0.5at^2
+        distance = (self.speed * dt) + (0.5 * GRAVITY * dt * dt)
+
+        # Calculate the speed with v = u + at
+        new_speed = self.speed + (GRAVITY * dt)
+
+        # Set the position of the bird on the y axis
+        self.rect.centery += distance
+
+        # Set speed of the bird
+        self.speed = new_speed
+
+        # If the bird goes above the screen
+        if self.rect.top < 0:
+            # Let the bird stay in screen
+            self.rect.top = 0
+            # Set the speed to 0
+            self.speed = 0
 
     def jump(self, pipes):
         """ Function to decide if the bird should flap or not
@@ -53,26 +98,59 @@ class Bird():
             self.speed = BIRD_START_SPEED
 
     def draw(self):
-        self.gameDisplay.blit(self.img, self.rect) #Draw every frame
+        """ Function to visualize the bird
+        """
 
-    def check_status(self, pipes): #Check if the bird still lives
-        if self.rect.bottom > DISPLAY_H: #If it is below the screen
+        # Draw every frame
+        self.gameDisplay.blit(self.img, self.rect)
+
+    def check_status(self, pipes):
+        """ Function to check if the bird is still alive
+
+        :param pipes: list, containing pipe information
+        """
+
+        # If the bird is below the screen
+        if self.rect.bottom > DISPLAY_H:
+            # Set the bird status as dead
             self.state = BIRD_DEAD
-        else: #if not
-            self.check_hits(pipes) #check if it hit with the pipes
+        # If the bird is still at the screen
+        else:
+            # Check if the bird hit a pipe
+            self.check_hits(pipes)
 
     def check_hits(self, pipes):
-        for p in pipes: #for every pipe in the list
-            if p.rect.colliderect(self.rect): #When the bird collides with a pipe
+        """ Function to check if the bird hits a pipe
+
+        :param pipes: list, containing pipe information
+        """
+
+        # For every pipe in the list
+        for p in pipes:
+            # When the bird collides with a pipe
+            if p.rect.colliderect(self.rect):
+                # Set the bird status as dead and stop the loop
                 self.state = BIRD_DEAD
                 break
 
     def update(self, dt, pipes):
-        if self.state == BIRD_ALIVE: #if alive, call all the actions below
+        """ Function to update the position of a living bird
+
+        :param dt: integer, time movement in milliseconds
+        :param pipes: list, containing pipe information
+        """
+
+        # Check if the bird is living
+        if self.state == BIRD_ALIVE:
+            # Update the living time of the bird
             self.time_lived += dt
+            # Update the position of the bird
             self.move(dt)
+            # Draw the bird at the new position
             self.draw()
+            # Check if the bird hits a pipe with the new position
             self.check_status(pipes)
+            # Check if the bird needs to flap
             self.jump(pipes)
 
     def get_inputs(self, pipes):
@@ -142,128 +220,129 @@ class Bird():
         self.time_lived = 0
         self.set_position(BIRD_START_X, BIRD_START_Y)
 
+
 class BirdCollection:
-   """ Object to store information of the bird collection
-   """
-
-   def __init__(self, gameDisplay):
-       """ Initialize the bird collection class
-
-       :param gameDisplay: pygame window size
+    """ Object to store information of the bird collection
        """
 
-       # Set the display width and height
-       self.gameDisplay = gameDisplay
+    def __init__(self, gameDisplay):
+        """ Initialize the bird collection class
 
-       # Store created birds
-       self.birds = []
+        :param gameDisplay: pygame window size
+        """
 
-       # When the class is initialized, an new generation of birds will be created
-       self.create_new_generation()
+        # Set the display width and height
+        self.gameDisplay = gameDisplay
 
-   def create_new_generation(self):
-       """ Function to create a new generation of birds
-       """
+        # Store created birds
+        self.birds = []
 
-       # Start with an empty bird list
-       self.birds = []
+        # When the class is initialized, an new generation of birds will be created
+        self.create_new_generation()
 
-       # Add as many birds as the defined population size
-       for i in range(0, GENERATION_SIZE):
-           # Extend the bird list with the new birds
-           self.birds.append(Bird(self.gameDisplay))
+    def create_new_generation(self):
+        """ Function to create a new generation of birds
+        """
 
-   def update(self, dt, pipes):
-       """
+        # Start with an empty bird list
+        self.birds = []
 
-       :param dt: integer, time movement in milliseconds
-       :param pipes: list, containing pipe information
-       :return:
-           num_alive: integer, number of living birds
-       """
+        # Add as many birds as the defined population size
+        for i in range(0, GENERATION_SIZE):
+            # Extend the bird list with the new birds
+            self.birds.append(Bird(self.gameDisplay))
 
-       # Start the counter of alive birds at 0
-       num_alive = 0
+    def update(self, dt, pipes):
+        """
 
-       # Check every bird in the bird list
-       for b in self.birds:
-           # Update the position of the bird by adding the movement and pipe information
-           b.update(dt, pipes)
-           # Check if the bird is living
-           if b.state == BIRD_ALIVE:
-               # Count the living birds
-               num_alive += 1
+        :param dt: integer, time movement in milliseconds
+        :param pipes: list, containing pipe information
+        :return:
+            num_alive: integer, number of living birds
+        """
 
-       # Return the number of living birds
-       return num_alive
+        # Start the counter of alive birds at 0
+        num_alive = 0
 
-   def evolve_population(self):
-       """ Function to evolve the birds each generation
-       """
+        # Check every bird in the bird list
+        for b in self.birds:
+            # Update the position of the bird by adding the movement and pipe information
+            b.update(dt, pipes)
+            # Check if the bird is living
+            if b.state == BIRD_ALIVE:
+                # Count the living birds
+                num_alive += 1
 
-       # For every bird in the list
-       for b in self.birds:
-           # Update the bird fitness determined by living time and the distance to the gap of the pipes
-           b.fitness += b.time_lived * PIPE_SPEED
+        # Return the number of living birds
+        return num_alive
 
-       # Sort the birds by their fitness
-       self.birds.sort(key=lambda x: x.fitness, reverse=True)
+    def evolve_population(self):
+        """ Function to evolve the birds each generation
+        """
 
-       # Set the cut off value
-       cut_off = int(len(self.birds) * MUTATION_CUT_OFF)
+        # For every bird in the list
+        for b in self.birds:
+            # Update the bird fitness determined by living time and the distance to the gap of the pipes
+            b.fitness += b.time_lived * PIPE_SPEED
 
-       # Set the good birds which are the top performing birds
-       good_birds = self.birds[0:cut_off]
+        # Sort the birds by their fitness
+        self.birds.sort(key=lambda x: x.fitness, reverse=True)
 
-       # Set the bad birds, which is the rest
-       bad_birds = self.birds[cut_off:]
+        # Set the cut off value
+        cut_off = int(len(self.birds) * MUTATION_CUT_OFF)
 
-       # Set the number of bad birds that should be kept
-       num_bad_to_take = int(len(self.birds) * MUTATION_BAD_TO_KEEP)
+        # Set the good birds which are the top performing birds
+        good_birds = self.birds[0:cut_off]
 
-       # For every bad bird
-       for b in bad_birds:
-           # Modify the neural network weights
-           b.nnet.modify_weight()
+        # Set the bad birds, which is the rest
+        bad_birds = self.birds[cut_off:]
 
-       # Create a list of new birds that are needed for every deleted bad bird
-       new_birds = []
+        # Set the number of bad birds that should be kept
+        num_bad_to_take = int(len(self.birds) * MUTATION_BAD_TO_KEEP)
 
-       # Pick the birds to take to the next round
-       idx_bad_to_take = np.random.choice(np.arange(len(bad_birds)), num_bad_to_take, replace=False)
+        # For every bad bird
+        for b in bad_birds:
+            # Modify the neural network weights
+            b.nnet.modify_weight()
 
-       # Add the birds to take to the next round to the bird list
-       for index in idx_bad_to_take:
-           new_birds.append(bad_birds[index])
+        # Create a list of new birds that are needed for every deleted bad bird
+        new_birds = []
 
-           # Extend the new bird list with the good birds to keep
-       new_birds.extend(good_birds)
+        # Pick the birds to take to the next round
+        idx_bad_to_take = np.random.choice(np.arange(len(bad_birds)), num_bad_to_take, replace=False)
 
-       # When more birds are needed
-       while len(new_birds) < len(self.birds):
+        # Add the birds to take to the next round to the bird list
+        for index in idx_bad_to_take:
+            new_birds.append(bad_birds[index])
 
-           # Pick random parents from the good birds
-           idx_to_breed = np.random.choice(np.arange(len(good_birds)), 2, replace=False)
+            # Extend the new bird list with the good birds to keep
+        new_birds.extend(good_birds)
 
-           # Make sure the parent birds are not the same
-           if idx_to_breed[0] != idx_to_breed[1]:
+        # When more birds are needed
+        while len(new_birds) < len(self.birds):
 
-               # Create a offspring bird from the two parent birds
-               new_bird = Bird.create_offspring(good_birds[idx_to_breed[0]], good_birds[idx_to_breed[1]],
-                                                self.gameDisplay)
+            # Pick random parents from the good birds
+            idx_to_breed = np.random.choice(np.arange(len(good_birds)), 2, replace=False)
 
-               # There is a chance that the weights of the neural network are changed
-               if random.random() < MUTATION_MODIFY_CHANCE_LIMIT:
-                   new_bird.nnet.modify_weight()
+            # Make sure the parent birds are not the same
+            if idx_to_breed[0] != idx_to_breed[1]:
 
-               # Add the created bird to the list
-               new_birds.append(new_bird)
+                # Create a offspring bird from the two parent birds
+                new_bird = Bird.create_offspring(good_birds[idx_to_breed[0]], good_birds[idx_to_breed[1]],
+                                                 self.gameDisplay)
 
-       # Reset the birds for every bird in the new list
-       for b in new_birds:
-           b.reset();
+                # There is a chance that the weights of the neural network are changed
+                if random.random() < MUTATION_MODIFY_CHANCE_LIMIT:
+                    new_bird.nnet.modify_weight()
 
-       # Update the bird list
-       self.birds = new_birds
+                # Add the created bird to the list
+                new_birds.append(new_bird)
+
+        # Reset the birds for every bird in the new list
+        for b in new_birds:
+            b.reset();
+
+        # Update the bird list
+        self.birds = new_birds
 
 
